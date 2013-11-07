66509359c160509de3dded7d0007e6ea90ee17b5
Zaccary Price <wildtangomedia@gmail.com>
2013-11-07 15:30:06 +0200

    smooth scrolling for hash links

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -44,3 +44,25 @@ $('#menu-dropdown-button').click(function(e) {
 	$('#top').toggleClass('open');
 });
 
+/*
+$('.nav-link').click(function(e) {
+	e.preventDefault();
+	var target = $(this).attr('href');
+	console.log(target);
+});*/
+
+$(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
+
